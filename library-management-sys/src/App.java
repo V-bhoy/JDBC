32d0af16cae10737b81fc48da51be80ccf7e278d
VBhoy <83490034+V-bhoy@users.noreply.github.com>
2025-03-20 14:57:44 +0530

    establish a mysql jdbc connection

---
+++ library-management-sys/src/App.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Objects;
+
+public class App {
+    private static final String DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/library";
+    private static final String USERNAME  = "root";
+    private static final String PASSWORD = "root";
+    public static void main(String[] args) throws Exception {
+        Connection connection = null;
+        try {
+            Class.forName(DRIVER_CLASS_NAME);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Established connection successfully!");
+        }catch(ClassNotFoundException | SQLException e){
+             e.printStackTrace();
+         } finally {
+             try {
+                 if (Objects.nonNull(connection)) {
+                     connection.close();
+                 }
+             } catch (SQLException e) {
+                e.printStackTrace();
+            }
+         
+        }
+    }
+}
